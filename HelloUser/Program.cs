fffa264b21f8ae9460ceb6527a05698b49695a31
dzz_cat <dzz_cat@mail.ru>
2023-04-05 15:20:08 +0400

    изменение записи вывода в одну строку вместо Console.WriteLine записываем Console.Write

--- HelloUser/Program.cs
+++ HelloUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿// выводим имя пользователя с предложением записать имя в строку
 Console.WriteLine("Введите Ваше имя");
 string username = Console.ReadLine();
-Console.WriteLine("Привет,");
-Console.WriteLine(username);
+Console.Write("Привет, ");
+Console.Write(username);
