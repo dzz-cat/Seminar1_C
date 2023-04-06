8ff8546ccc3974c90d2606d07230c525b80497ff
dzz_cat <dzz_cat@mail.ru>
2023-04-06 12:01:22 +0400

    .gitignore is now working

--- HelloUser/Program.cs
+++ HelloUser/Program.cs
@@ -1,5 +1,14 @@
-﻿// выводим имя пользователя с предложением записать имя в строку
-Console.WriteLine("Введите Ваше имя");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // выводим имя пользователя с предложением записать имя в строку
+        Console.WriteLine("Введите Ваше имя");
+
+        static string? GetUsername() => Console.ReadLine();
+
+        string username = GetUsername();
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
+}
